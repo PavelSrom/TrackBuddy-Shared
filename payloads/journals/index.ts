fef72770b3a06ce3bbe26c52f3d119ad0e32ab65
Pavel Srom <srompavel98@gmail.com>
2020-12-01 18:55:13 +0100

    new types for journals

---
+++ payloads/journals/index.ts
@@ -0,0 +1,9 @@
+type JournalFullASP = {
+  mood: 1 | 2 | 3 | 4 | 5
+  standout: string
+  wentWell: string
+  wentWrong: string
+  betterNextTime: string
+  excuses: string
+  tags: string[]
+}
---
+++ responses/journals/index.ts
@@ -0,0 +1,20 @@
+type JournalBriefASR = {
+  _id: string
+  isStarred: boolean
+  mood: 1 | 2 | 3 | 4 | 5
+  standout: string
+  tags: string[]
+}
+
+type JournalFullASR = {
+  _id: string
+  user: string
+  isStarred: boolean
+  mood: 1 | 2 | 3 | 4 | 5
+  standout: string
+  wentWell: string
+  wentWrong: string
+  betterNextTime: string
+  excuses: string
+  tags: string[]
+}
