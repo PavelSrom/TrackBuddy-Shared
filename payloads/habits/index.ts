26d0b04e4dbe8deaf2e33af096b386ca6e8b68b1
Pavel Srom <srompavel98@gmail.com>
2021-01-01 17:35:05 +0100

    feat(habits): habit types

---
+++ payloads/habits/index.ts
@@ -0,0 +1,6 @@
+export interface HabitNewASP {
+  name: string
+  color: string
+  duration: number
+  frequency: number
+}
---
+++ responses/habits/index.ts
@@ -0,0 +1,16 @@
+export interface HabitOverviewASR {
+  _id: string
+  name: string
+  color: string
+  duration: number
+  frequency: number
+  newestRep: number
+}
+
+export interface HabitFullASR {
+  _id: string
+  name: string
+  color: string
+  duration: number
+  frequency: number
+}
